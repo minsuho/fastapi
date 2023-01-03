7fce8fa9b69d212cb026fdce69bca0da7e046c48
suho min <58880552+minsuho@users.noreply.github.com>
2023-01-03 11:52:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ app = FastAPI()
 async def root():
     return {}
 
-@app.get("/VideosSearch/")
+@app.get("/youtube/VideosSearch/")
 async def VideosSearch1(q: str = None, n: int = 1):
     videosSearch = VideosSearch(q, limit = n, region = 'KO')
     data = videosSearch.result()
     return data
 
-@app.get("/videofullInfo/")
+@app.get("/youtube/videofullInfo/")
 async def videofullInfo(id: str = None):
     video = Video.get(id, mode = ResultMode.json, get_upload_date=True)
     return video
 
-@app.get("/videoInfo/")
+@app.get("/youtube/videoInfo/")
 async def videoInfo(id: str = None):
     video = Video.getInfo(id, mode = ResultMode.json)
     return video
